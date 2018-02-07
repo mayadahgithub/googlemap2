4b4c6e26e6605f1b2c5f44fd1394644a2876c064
mayadahgithub <mayadahnaji@gmail.com>
2018-02-07 15:29:09 +0300

    add googlemap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,10 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+ ////add
+import { HttpModule } from '@angular/http';
+
+
 @NgModule({
   declarations: [
     MyApp,
@@ -21,7 +25,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,24 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, trigger } from '@angular/core';
+import { NavController, Platform} from 'ionic-angular';
+
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  CameraPosition,
+  MarkerOptions,
+  Marker
+ } from '@ionic-native/google-maps';
+
+
+///add
+ import { Http } from '@angular/http';
+import'rxjs/add/operator/map';
+
+declare var google;
+///
+ 
 
 @Component({
   selector: 'page-home',
@@ -7,8 +26,80 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  map: GoogleMap;
+
+  constructor(public navCtrl: NavController,public platform:Platform,private http: Http) {
+
+    platform.ready().then(()=>{
+      this.loadmap();
+    })
 
   }
+
+    loadmap(){
+    this.map = new GoogleMap('map',{
+     'controls': {
+    'compass':true,
+    'myLocationButton':true,
+    'indoorPicker':true,
+    'zoom':true
+     },
+     'gestures':{
+       'scroll':true,
+       'tilt':true,
+       'rotate':true,
+       'zoom':true
+     },
+     'camera':{
+      target: {
+        lat: 43.0741904,
+        lng: -89.3809802
+      },
+      zoom: 18,
+      tilt: 30,
+      'bearing':50
+     
+     }
+    });
+    this.map.one(GoogleMapsEvent.MAP_READY).then(()=>{
+      console.log('Map is ready .... :)');
+    })
+   }
+    
+  
+///add
+
+ionviewDidload(){
+ // this.displayGoogleMap();
+  this.getMarkers();
+}
+
+
+
+
+
+getMarkers(){
+  this.http.get('assets\data\markers.json').map((res)=>res.json()).subscribe(data=>{
+    this.addMarkersMap(data);
+  }); 
+}
+
+addMarkersMap(markers){
+for(let marker of markers){
+  var loc ={lat:marker.latitude ,lng:marker.longitude}
+  marker = new google.maps.Marker({
+    position : loc,
+    map: this.map,
+    title:marker.name,
+    label:marker.content
+
+  });
+
+}
+}
+////
+
+
+
 
 }
